39a2f587e84e3e943f7061feb8858241be0294fd
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-11-18 21:58:40 +0000

    Basic snips functionality

---
+++ snips.ai/README.md
@@ -0,0 +1,26 @@
+# Snips.ai (Hass.io Addon)
+![armhf image pulls](https://img.shields.io/docker/pulls/poeschl/hassio-snipsai-armhf?label=docker%20pulls%20%28armhf%29)
+![armv7 image pulls](https://img.shields.io/docker/pulls/poeschl/hassio-snipsai-armv7?label=docker%20pulls%20%28armv7%29)
+![aarch64 image pulls](https://img.shields.io/docker/pulls/poeschl/hassio-snipsai-aarch64?label=docker%20pulls%20%28aarch64%29)
+![amd64 image pulls](https://img.shields.io/docker/pulls/poeschl/hassio-snipsai-amd64?label=docker%20pulls%20%28amd64%29)
+
+A own version of the Snips.ai Voice Control platform addon. Its intended to act only as base station without a mic and gets the input via satelites.
+
+## Requirements
+
+To use this Addon a MQTT Broker is required. The easy solution for that is the [Mosquitto broker addon](https://www.home-assistant.io/addons/mosquitto/).
+
+## Configuration
+
+```
+{
+    "mqtt": {
+        "host": "localhost",
+        "port": 1883
+        "user": "user",
+        "pass": "password",
+    },
+    "assistant": "mySnipsAssistant.zip"
+}
+```
+
---
+++ snips.ai/run.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bashio
+set -e
+set +u
+
+bashio::log.info "Setup Settings"
+
+broker_host=$(bashio::config 'mqtt.host')
+broker_port=$(bashio::config 'mqtt.port')
+broker_user=$(bashio::config 'mqtt.user')
+broker_pass=$(bashio::config 'mqtt.password')
+
+sed -i "/^# mqtt =/s/^# //" /etc/snips.toml
+sed -i "/^mqtt =/s/=.*/= \"${broker_host}:${broker_port}\"/" /etc/snips.toml
+sed -i "/^# mqtt_username =/s/^# //" /etc/snips.toml
+sed -i "/^mqtt_username =/s/=.*/= \"${broker_user}\"/" /etc/snips.toml
+sed -i "/^# mqtt_password =/s/^# //" /etc/snips.toml
+sed -i "/^mqtt_password =/s/=.*/= \"${broker_pass}\"/" /etc/snips.toml
+
+sed -i "/^# bind =/s/^# //" /etc/snips.toml
+sed -i "/^bind =/s/=.*/= \"base@mqtt\"/" /etc/snips.toml
+sed -i "/^# disable_capture =/s/^# //" /etc/snips.toml
+sed -i "/^disable_capture =/s/=.*/= true/" /etc/snips.toml
+
+
+bashio::log.info "Setup Assistant"
+assistant_path="/share/$(bashio::config 'assistant')"
+bashio::fs.file_exists "$assistant_path" || (bashio::log.error "Assistant zip not found! Expected location: '$assistant_path'"; exit 1)
+
+bashio::log.info "| Unzip Assistant..."
+unzip -q "$assistant_path" -d /usr/share/snips
+bashio::log.info "| Load Skill configs..."
+mkdir -p /share/snips-skills
+cp -r /share/snips-skills /var/lib/snips/skills
+bashio::log.info "| Generate Skill templates..."
+ln -s /share/snips-skills /var/lib/snips/skills
+snips-template render
+
+pushd /var/lib/snips/skills > /dev/null
+
+for url in $(awk '$1=="url:" {print $2}' /usr/share/snips/assistant/Snipsfile.yaml); do
+  bashio::log.info "| Clone $url..."
+  git clone "$url"
+done
+
+find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' dir; do
+    pushd "$dir" > /dev/null
+    if [ -f setup.sh ]; then
+        bashio::log.info "| Run setup.sh for '$dir'"
+        python3 -m venv venv
+        source venv/bin/activate
+        pip3 install --upgrade pip setuptools wheel
+        bash ./setup.sh
+    fi
+    popd > /dev/null
+done
+
+chown -R _snips-skills:_snips-skills /var/lib/snips/skills
+popd > /dev/null
+
+bashio::log.info "| Save Skill config..."
+find /var/lib/snips/skills/ -name 'config*' -exec cp {} /share/snips-skills \;
+
+bashio::log.info "Start snips-asr"
+snips-asr > /dev/null || bashio::log.error "Snips-ASR service failed!" &
+bashio::log.info "Start snips-audio-server"
+snips-audio-server > /dev/null || bashio::log.error "Snips-Audio-Server service failed!" &
+bashio::log.info "Start snips-dialogue"
+snips-dialogue > /dev/null || bashio::log.error "Snips-Dialogue service failed!" &
+bashio::log.info "Start snips-nlu"
+snips-nlu > /dev/null || bashio::log.error "Snips-NLU service failed!" &
+bashio::log.info "Start snips-skill-server"
+snips-skill-server > /dev/null || bashio::log.error "Snips-Skill-Server service failed!" &
+
+bashio::log.info "Start snips-watch"
+snips-watch -vv --no_color && kill $(jobs -p)
