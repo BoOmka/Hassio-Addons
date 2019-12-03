bbed32e69915e606873ffb60e6e9be88119de9c0
Boomka <zhilin.alx@gmail.com>
2019-12-03 14:17:51 +0300

    log config.json

--- ICantBelieveItsNotValetudo/run.sh
+++ ICantBelieveItsNotValetudo/run.sh
@@ -6,5 +6,8 @@ echo '{"mqtt":' > /app/config.json
 cat /data/options.json >> /app/config.json
 echo ', "webserver": { "enabled": true, "port": 3000 }}' >> /app/config.json
 
+bashio::log.info 'Config:'
+cat /app/config.json | bashio::log.info
+
 bashio::log.info 'Start ICantVelieveItsNotValetudo'
 npm start
